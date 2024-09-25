8b716c4005d4bacb15b978ed11fc8bc8ddce9a2e
rahul <rsharma346@myseneca.ca>
2024-09-25 12:14:05 -0400

    Added lab1d

---
+++ lab1d.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# Assign values to the objects
+x = 10
+y = 2
+z = 5
+
+# Perform the calculation and print the result
+result = x + y * z
+print(f'{x} + {y} * {z} = {result}')
+
