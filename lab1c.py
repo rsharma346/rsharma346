aeed339754687b0d725256b62cfe4170299ccef4
rahul <rsharma346@myseneca.ca>
2024-09-25 12:11:37 -0400

    Added lab1c

---
+++ lab1c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+# Assign values to the objects
+name = 'Isaac'
+age = 72
+
+# Print the output using concatenation and the str() function
+print(name + ' is ' + str(age) + ' years old!')
+
